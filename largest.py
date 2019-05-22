56f488c202fba338585139ead20245a8fb6195ae
vsmac <vsoodles@gmail.com>
2019-05-22 11:43:06 +0530

    update the code

---
+++ largest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Large:
+    def large(self):
+        a = 0
+        for i in list:
+            if i > a:
+                a = i
+        print("largest no. is:",a)
+
+print("enter the no. of element in list")
+list = [int(x) for x in input().split()]
+
+p1 = Large()
+p1.large()
