90d280a7cf83e36cb5735576ffddafe65c0471ef
vsmac <vsoodles@gmail.com>
2019-05-27 11:46:11 +0530

    update curl.py

---
+++ curl.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import os
+for i in range(0,11):
+   os.system('curl -IS https://www.google.com')
+
---
+++ hi.py
@@ -0,0 +1,2 @@
+print('we are trying to practice at git stash and git stash pop')
+
